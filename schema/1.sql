9177363d1adac1b3bc9b7b6d33c2a2be802d5bd2
Don Park <donp@donp.org>
2017-05-09 10:52:48 -0700

    over-monkeying for an optional username

--- schema/1.sql
+++ schema/1.sql
@@ -2,6 +2,5 @@ create table if not exists accounts (
   id varchar(128) primary key,
   fullname text,
   email text not null unique,
-  username text unique
+  username text
 );
-
--- src/actions/signup_form.go
+++ src/actions/signup_form.go
@@ -2,6 +2,7 @@ package actions
 
 import (
 	"log"
+	"strings"
 
 	"db"
 	"proto"
@@ -18,45 +19,47 @@ func DoSignupform(form proto.SignupForm, json string) proto.SignupFormResponse {
 
 	rows, _ := db.D.Handle.Query(
 		"select count(*) from accounts where email = $1",
-		form.Account.GetEmail())
+		form.Account.Email)
 
 	if rows.Next() {
 		var count int
 		rows.Scan(&count)
 		if count > 0 {
-			log.Printf("email check %+v", count)
+			log.Printf("email check %d", count)
 			return proto.SignupFormResponse{Ok: false, Reason: proto.SignupFormResponse_EMAIL_TAKEN}
 		}
 	}
 
-	rows, _ = db.D.Handle.Query(
-		"select count(*) from accounts where username is not null and username = $1",
-		form.Account.GetUsername())
-	if rows.Next() {
-		var count int
-		rows.Scan(&count)
-		if count > 0 {
-			log.Printf("username check %+v", count)
-			return proto.SignupFormResponse{Ok: false, Reason: proto.SignupFormResponse_USERNAME_TAKEN}
+	if len(strings.TrimSpace(form.Account.Username)) > 0 {
+		rows, _ = db.D.Handle.Query(
+			"select count(*) from accounts where username = $1",
+			form.Account.Username)
+		if rows.Next() {
+			var count int
+			rows.Scan(&count)
+			if count > 0 {
+				log.Printf("username check %d", count)
+				return proto.SignupFormResponse{Ok: false, Reason: proto.SignupFormResponse_USERNAME_TAKEN}
+			}
 		}
 	}
 
-	rows, err = db.D.Handle.Query(
-		"insert into accounts (id, email, username, fullname) values ($1, $2, $3, $4)",
+	stmt, err := db.D.Handle.Prepare("insert into accounts (id, fullname, email) values ($1, $2, $3)")
+	if err != nil {
+		log.Printf("prepare %+v", err)
+		return proto.SignupFormResponse{Ok: false}
+	}
+	sql_result, err := stmt.Exec(
 		db.NewId("accounts"),
-		form.Account.GetEmail(),
-		form.Account.GetUsername(),
-		form.Account.GetFullname())
+		form.Account.Fullname,
+		form.Account.Email)
 	if err != nil {
 		log.Printf("upsert %+v", err)
 		return proto.SignupFormResponse{Ok: false}
 	}
-	cols, err := rows.Columns()
-	if err == nil {
-		log.Printf("rows %+v", cols)
-	} else {
-		log.Printf("err %s", err)
-	}
+	new_id, err := sql_result.LastInsertId()
+	log.Printf("new id %s", new_id)
+
 	resp := proto.SignupFormResponse{Ok: true}
 	return resp
 }
