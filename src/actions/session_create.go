b915b32e39d73e0ad89a180e373d09505d7488c7
Don Park <donp@donp.org>
2017-05-10 11:05:17 -0700

    close the cursor

--- src/actions/session_create.go
+++ src/actions/session_create.go
@@ -15,10 +15,12 @@ func DoSessionCreate(sessionCreate proto.SessionCreate, json string) []interface
 		log.Print("unmarshaling error: ", err)
 		return []interface{}{proto.SessionCreateResponse{Ok: false}}
 	}
+
+	var responses []interface{}
 	rows, err := db.D.Handle.Query("select account_id from tokens where token = $1", sessionCreate.Token)
 	if err != nil {
 		log.Print("token sql error: ", err)
-		return []interface{}{proto.SessionCreateResponse{Ok: false}}
+		responses = append(responses, proto.SessionCreateResponse{Ok: false})
 	}
 
 	if rows.Next() {
@@ -29,20 +31,21 @@ func DoSessionCreate(sessionCreate proto.SessionCreate, json string) []interface
 		rows2, err := db.D.Handle.Query("select fullname, email from accounts where id = $1", accountId)
 		if err != nil {
 			log.Print("token sql error: ", err)
-			return []interface{}{proto.SessionCreateResponse{Ok: false}}
-		}
-		if rows2.Next() {
-			var fullname string
-			var email string
-			rows2.Scan(&fullname, &email)
-			log.Printf("account found %s %s", fullname, email)
-			acct := proto.Account{Fullname: fullname, Email: email}
-			return []interface{}{proto.SessionCreateResponse{Ok: true, Account: &acct}}
+			responses = append(responses, proto.SessionCreateResponse{Ok: false})
 		} else {
-			return []interface{}{proto.SessionCreateResponse{Ok: true}}
+			if rows2.Next() {
+				var fullname string
+				var email string
+				rows2.Scan(&fullname, &email)
+				log.Printf("account found %s %s", fullname, email)
+				acct := proto.Account{Fullname: fullname, Email: email}
+				responses = append(responses, proto.SessionCreateResponse{Ok: true, Account: &acct})
+			} else {
+				log.Printf("toekn lookup no next", rows)
+				responses = append(responses, proto.SessionCreateResponse{Ok: false})
+			}
 		}
-	} else {
-		log.Printf("toekn lookup no next", rows)
-		return []interface{}{proto.SessionCreateResponse{Ok: false}}
 	}
+	rows.Close()
+	return responses
 }
