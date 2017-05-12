88d1e1c7888409104cc5e7a60e80363124f82a16
Don Park <donp@donp.org>
2017-05-12 12:09:11 -0700

    fancier mail

--- src/mailer/mailer.go
+++ src/mailer/mailer.go
@@ -1,16 +1,22 @@
 package mailer
 
-import "net/smtp"
 import "log"
 import "config"
+import "gopkg.in/gomail.v2"
 
 func MailToken(to string, token string) {
 	log.Printf("emailing %s %s", to, token)
-	err := smtp.SendMail(config.C.QueryString("email.smtp"),
-		nil,
-		config.C.QueryString("email.from"),
-		[]string{to},
-		[]byte("msg "+token))
+
+	m := gomail.NewMessage()
+	m.SetHeader("From", config.C.QueryString("email.from"))
+	m.SetHeader("To", to)
+	//m.SetAddressHeader("Cc", "dan@example.com", "Dan")
+	m.SetHeader("Subject", "Web Login")
+	m.SetBody("text/html", "Hello <b>Bob</b> and <i>Cora</i>!")
+
+	d := gomail.NewDialer(config.C.QueryString("email.smtp"), 25, "", "")
+
+	err := d.DialAndSend(m)
 	if err != nil {
 		log.Print(err)
 	}
